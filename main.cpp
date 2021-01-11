ce228a76dbe41714f83b5af466889bc2d526c745
jordan <scherring.jordan@gmail.com>
2021-01-11 17:58:17 +0100

    Ajout de la lecture des faces

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include "tgaimage.h"
 
-using namespace std;
+
 
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
@@ -14,11 +14,17 @@ const TGAColor green = TGAColor(0, 255, 0, 255);
 
 struct vecteur
 {
-    string vec[3];
+   std::string vec[3];
+};
+
+struct face
+{
+    std::vector<int> faces;
 };
 
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
+
+void lines(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     
     bool steep = false;
     if ( std::abs(x0-x1) < std::abs(y0-y1) ) {
@@ -59,35 +65,44 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
-	
-    line(13, 20, 80, 40, image, white);
-    line(20, 13, 40, 80, image, red);
-    line(80, 40, 13, 20, image, blue);
 
-    ifstream fichier("obj/african_head/african_head.obj", ios::in);
+    std::ifstream fichier("obj/african_head/african_head.obj", std::ios::in);
 
-    string line;
-    vector<vecteur> vecteurs;
+    std::string line;
+    std::vector<vecteur> vecteurs;
+    std::vector<face> faces;
     
     while (!fichier.eof()){
         getline(fichier,line);
+        std::istringstream iss(line);
+        std::string temp;
 
         if (!line.compare(0, 2, "v ")){
-            istringstream iss(line);
-            string temp;
             vecteur v;
             iss >> temp;
-            iss >> v.vec[0];
-            iss >> v.vec[1];
-            iss >> v.vec[2];
+            for (int i = 0; i < 3; i++) {
+                iss >> v.vec[i];
+            }
             vecteurs.push_back(v);
+
+        } else if (!line.compare(0, 2, "f ")) {
+            face f;
+
+            int idx, itrash;
+            iss >> temp;
+            
+            while (iss >> idx >> temp >> itrash >> temp >> itrash)
+            {
+                idx--;
+                f.faces.push_back(idx);
+               
+            }
+            faces.push_back(f);
+            
         }
     }
 
-    for (int i = 0; i < vecteurs.size(); i++)
-    {
-        cout << vecteurs[i].vec[0] << vecteurs[i].vec[1] << vecteurs[i].vec[2] << endl;
-    }
+    
     
 
     
