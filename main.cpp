7af60e7bf788731ea8a04963f54c1baf50c7f0e9
jordan <scherring.jordan@gmail.com>
2021-02-17 21:23:27 +0100

    delete methode lines

--- main.cpp
+++ main.cpp
@@ -82,46 +82,6 @@ Matrix lookat(Vecteur eye, Vecteur center, Vecteur up) {
 
 }
 
-void lines(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-    
-    bool steep = false;
-    if ( std::abs(x0-x1) < std::abs(y0-y1) ) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
-        steep = true;
-    }
-
-    if ( x0 > x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
-    }
-
-    int dx = x1-x0;
-    int dy = y1-y0;
-
-    int derror = std::abs(dy)*2;
-    int error = 0;
-    int y = y0;
-
-    for (int x = x0; x <= x1; x++) {
-        if (steep) {
-            image.set(y, x, color);
-
-        } else {
-            image.set(x, y, color);
-
-        }
-        error += derror;
-        if (error > dx) {
-            y += ( y1 > y0 ? 1 : -1);
-            error -= dx*2;
-        } 
-    }
-
-
-}
-
-
 Vecteur barycentric(Vecteur *pts,  Pointi P) {
    
     Vecteur temp1(pts[2].x - pts[0].x, pts[1].x - pts[0].x, pts[0].x - P.x);
